cf321447ea65547b645f5a39d33f7a30780f56e6
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-11-15 00:01:56 +0300

    bre

--- projec/projec/settings.py
+++ projec/projec/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =  os.getenv('SECRET_KEY')
+SECRET_KEY =  os.getenv('SECRET_KEY', "django-insecure-x^k-0m9(d1zfs4g$#$6$y66go5e^74-@p*zfn7cn!%)1bi_bwn")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
