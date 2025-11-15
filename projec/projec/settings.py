ae7bd34f6878b430a0fefcceb6a8e05ad4d5c60d
Dim4ik-creator <141348723+Dim4ik-creator@users.noreply.github.com>
2025-11-15 13:12:50 +0300

    time_zone

--- projec/projec/settings.py
+++ projec/projec/settings.py
@@ -100,9 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = ''
+LANGUAGE_CODE = 'ru-ru'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
